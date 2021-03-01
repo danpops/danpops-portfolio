bf2fdb9762234491d5ab51ddcae609fb5d32b0a4
Daniel Popovic <dan-popovic@hotmail.com>
2021-03-01 13:54:01 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
                             <ul className="navbar-nav ml-auto my-2 my-lg-0">
                                 <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">about</a></li>
                                 <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#services">skills</a></li>
-                                <li classxName="nav-item"><a className="nav-link js-scroll-trigger" href="#portfolio">projects</a></li>
+                                <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#portfolio">projects</a></li>
                                 <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#contact">contact</a></li>
                             </ul>
                         </div>
