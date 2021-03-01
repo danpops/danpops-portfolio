9bdcb04ec0f2145d4dd5ec7be30f393d3823809c
Daniel Popovic <dan-popovic@hotmail.com>
2021-03-01 14:14:04 -0500

    c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import Header from './components/Header'
 import About from './components/About'
 import Skills from './components/Skills'
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,10 +12,8 @@ export default class Header extends Component {
                                 <h2 className="text-white font-weight-bold">i'm daniel popovic!</h2>
                                 <hr className="divider my-4" />
                             </div>
+                            
                             <div className="col-lg-8 align-self-baseline">
-                                <a href="" className="typewrite text-white-75 font-weight-light mb-5" data-period="2000" data-type='[ "software engineer.", "ux designer.", "student.", "innovator." ]'>
-                                    <span className="wrap"></span>
-                                </a><br /><br />
                                 <a className="btn btn-primary btn-xl js-scroll-trigger text-lowercase" href="#about">who am i?</a>
                             </div>
                         </div>
