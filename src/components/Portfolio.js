9460c5a0cc7739482de8ca176dc015646d430bc3
Dan Popovic <59669570+danpops@users.noreply.github.com>
2021-05-08 12:19:57 -0400

    fixed effects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react'
 import PortfolioCard from "./PortfolioCard"
 import data from "../data/projects.json"
-import Wobble from 'react-reveal/Wobble';
+import Bounce from 'react-reveal/Bounce';
 
 export default class Portfolio extends Component {
 
     render() {
         return (
             <div id="portfolio">
-                    <section className="page-section bg-dark text-white">
-                        <div className="container text-center">
-                            <Wobble>
-                                <i className="fab fa-6x fa-github text-white mb-4"></i>
-                                <h2 className="mb-4">check out my work on github!</h2>
-                                <a className="btn btn-light btn-xl text-lowercase" href="https://github.com/danpops">github/danpops</a>
-                            </Wobble>
-                        </div>
-                    </section>
+                <section className="page-section bg-dark text-white">
+                    <div className="container text-center">
+                        <i className="fab fa-6x fa-github text-white mb-4"></i>
+                        <Bounce bottom>
+                            <h2 className="mb-4">check out my work on github!</h2>
+                            <a className="btn btn-light btn-xl text-lowercase" href="https://github.com/danpops">github/danpops</a>
+                        </Bounce>
+                    </div>
+                </section>
 
 
-                    <div className="container-fluid p-0">
-                        <div className="row no-gutters">
+                <div className="container-fluid p-0">
+                    <div className="row no-gutters">
                         {data.map((project, index) => (
-                            <PortfolioCard 
-                                key={index} 
+                            <PortfolioCard
+                                key={index}
                                 location={project.location}
                                 image={project.image}
                                 title={project.title}
                                 description={project.description}
                             />
-                        ))} 
-                        </div>
+                        ))}
                     </div>
+                </div>
             </div>
         )
     }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
-import RubberBand from 'react-reveal/RubberBand';
-import Bounce from 'react-reveal/Bounce';
+import React, { Component } from 'react';
+import Zoom from 'react-reveal/Zoom';
 
 export default class Skills extends Component {
     render() {
@@ -8,46 +7,46 @@ export default class Skills extends Component {
             <div>
                 <section className="page-section" id="services">
                     <div className="container">
-                        <Bounce bottom>
+                        <Zoom>
                             <h2 className="text-center mt-0">what are my skills?</h2>
-                            <hr className="divider my-4" />
-                        </Bounce>
+                        </Zoom>
+                        <hr className="divider my-4" />
                         <div className="row">
                             <div className="col-lg-3 col-md-6 text-center">
-                                <RubberBand>
+                                <Zoom>
                                     <div className="mt-5">
                                         <i className="fas fa-4x fa-user-graduate text-primary mb-4"></i>
                                         <h3 className="h4 mb-2">knowledgeable</h3>
                                         <p className="text-muted mb-0">project management, solid understanding of programming fundamentals, usability testing</p>
                                     </div>
-                                </RubberBand>
+                                </Zoom>
                             </div>
                             <div className="col-lg-3 col-md-6 text-center">
-                                <RubberBand>
+                                <Zoom>
                                     <div className="mt-5">
                                         <i className="fas fa-4x fa-terminal text-primary mb-4"></i>
                                         <h3 className="h4 mb-2">programming</h3>
                                         <p className="text-muted mb-0">java, javascript, swift, python, oracle, sql, reactjs, jsp, unix</p>
                                     </div>
-                                </RubberBand>
+                                </Zoom>
                             </div>
                             <div className="col-lg-3 col-md-6 text-center">
-                                <RubberBand>
+                                <Zoom>
                                     <div className="mt-5">
                                         <i className="fas fa-4x fa-users text-primary mb-4"></i>
                                         <h3 className="h4 mb-2">team leader</h3>
                                         <p className="text-muted mb-0">leadership, problem-solving, communication, motivation</p>
                                     </div>
-                                </RubberBand>
+                                </Zoom>
                             </div>
                             <div className="col-lg-3 col-md-6 text-center">
-                                <RubberBand>
+                                <Zoom>
                                     <div className="mt-5">
                                         <i className="fas fa-4x fa-briefcase text-primary mb-4"></i>
                                         <h3 className="h4 mb-2">work ethic</h3>
                                         <p className="text-muted mb-0">rapid learner, thorough, analytical, entrepreneurial attitude</p>
                                     </div>
-                                </RubberBand>
+                                </Zoom>
                             </div>
                         </div>
                     </div>
