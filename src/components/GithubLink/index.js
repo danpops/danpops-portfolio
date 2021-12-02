e76b853dd3de6190613190112a094838755b3299
Dan Popovic <dpopovic@tribalscale.com>
2021-12-02 12:59:49 -0500

    fix: github href

--- src/components/GithubLink/index.js
+++ src/components/GithubLink/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Zoom from "react-reveal/Zoom";
 import { motion } from "framer-motion";
 import { FaGithub } from "react-icons/fa";
@@ -11,16 +11,17 @@ const GitHubLink = () => {
         <FaGithub size={90} className="text-white mb-4" />
         <Zoom>
           <h2 className="mb-4">check out my work on github!</h2>
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn-xl btn-light text-lowercase shadow-sm"
-            href="https://github.com/danpops"
-          >
-            github/danpops
-          </motion.button>
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <Button
+              size="xl"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn-light text-lowercase shadow-sm"
+              href="https://github.com/danpops"
+            >
+              github/danpops
+            </Button>
+          </motion.div>
         </Zoom>
       </Container>
     </section>
