f8aa8d2ad0cec2b5688dc1c338dbfd1ae50d5d3b
Dan Popovic <dpopovic@tribalscale.com>
2021-12-02 13:28:47 -0500

    update zoom effect

--- src/components/Specialties/SpecialtyContent/index.js
+++ src/components/Specialties/SpecialtyContent/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Col, Tab } from "react-bootstrap";
+import Zoom from "react-reveal/Zoom";
 
 import SpecialtyPane from "./SpecialtyPane";
 
 const SpecialtyContent = ({ data }) => {
   return (
-    <Col md={9} lg={8}>
-      <Tab.Content className="mx-5 mt-5 text-center">
-        {data.map((item, index) => (
-          <SpecialtyPane
-            key={index}
-            data={item.data}
-            eventKey={item.eventKey}
-          />
-        ))}
-      </Tab.Content>
-    </Col>
+    <Zoom>
+      <Col md={9} lg={8}>
+        <Tab.Content className="mx-5 mt-5 text-center">
+          {data.map((item, index) => (
+            <SpecialtyPane
+              key={index}
+              data={item.data}
+              eventKey={item.eventKey}
+            />
+          ))}
+        </Tab.Content>
+      </Col>
+    </Zoom>
   );
 };
 
--- src/components/Specialties/SpecialtyTabs/index.js
+++ src/components/Specialties/SpecialtyTabs/index.js
@@ -7,8 +7,8 @@ import { tabStyle } from "../styles";
 
 const SpecialtyTabs = ({ data, setKey }) => {
   return (
-    <Col md={3} lg={4}>
-      <Zoom>
+    <Zoom>
+      <Col md={3} lg={4}>
         <Nav
           variant="pills"
           className="flex justify-content-center align-items-start"
@@ -28,8 +28,8 @@ const SpecialtyTabs = ({ data, setKey }) => {
             </motion.div>
           ))}
         </Nav>
-      </Zoom>
-    </Col>
+      </Col>
+    </Zoom>
   );
 };
 
